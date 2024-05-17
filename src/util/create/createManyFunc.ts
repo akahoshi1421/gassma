600ea8ce41957ba3a2775c640d335833c08f6af3
akahoshi1421 <akaho1421@gmail.com>
2024-05-17 20:04:52 +0900

    change: createMany algolism changed

--- src/util/create/createManyFunc.ts
+++ src/util/create/createManyFunc.ts
@@ -1,20 +1,42 @@
 import { CreateData, CreateManyData } from "../../types/createTypes";
 import { GassmaControllerUtil } from "../../types/gassmaControllerUtilType";
-import { createFunc } from "./create";
+import { getTitle } from "../core/getTitle";
+import { getWantUpdateIndex } from "../core/getWantUpdateIndex";
 
 const createManyFunc = (
   gassmaControllerUtil: GassmaControllerUtil,
   createManyData: CreateManyData
 ) => {
+  const { sheet, startRowNumber, startColumNumber, endColumNumber } =
+    gassmaControllerUtil;
+
   const data = createManyData.data;
+  if (data.length === 0) return;
 
-  data.forEach((oneCreateData) => {
-    const create = {
-      data: oneCreateData,
-    } as CreateData;
+  const titles = getTitle(gassmaControllerUtil);
 
-    createFunc(gassmaControllerUtil, create);
+  const createdDataModel = {
+    data: data[0],
+  } as CreateData;
+
+  const wantCreateIndex = getWantUpdateIndex(
+    gassmaControllerUtil,
+    createdDataModel
+  ).sort();
+
+  const newData = data.map((row) => {
+    return wantCreateIndex.map((index) => {
+      return row[String(titles[index])];
+    });
   });
+
+  const rowNumber = sheet.getLastRow() + 1;
+  const columLength = endColumNumber - startColumNumber + 1;
+  const rowLength = newData.length;
+
+  sheet
+    .getRange(rowNumber, startColumNumber, rowLength, columLength)
+    .setValues(newData);
 };
 
 export { createManyFunc };
