7826495e069e198a09370fe544385a476154ab8f
akahoshi1421 <akaho1421@gmail.com>
2024-05-25 03:39:05 +0900

    add: ORを実装

--- src/util/andOrNot/or.ts
+++ src/util/andOrNot/or.ts
@@ -1,22 +1,71 @@
 import { WhereUse } from "../../types/coreTypes";
 import { GassmaControllerUtil } from "../../types/gassmaControllerUtilType";
+import { HitRowData } from "../../types/hitRowDataType";
 import { getWantFindIndex } from "../core/getWantFindIndex";
+import { isFilterConditionsMatch } from "../filterConditions/filterConditions";
+import { isDict } from "../other/isDict";
+import { isLogicMatch } from "./entry";
 
 const isOrMatch = (
-  rows: any[][],
+  rowsData: HitRowData[],
   whereArray: WhereUse[],
   titles: any[],
   gassmaControllerUtil: GassmaControllerUtil
 ) => {
-  let resultRows: any[][] = rows.concat();
+  let resultRowsData: HitRowData[] = rowsData.concat();
 
-  whereArray.forEach((where) => {
+  whereArray.forEach((where, whereArrayIndex) => {
     const wantFindIndex = getWantFindIndex(gassmaControllerUtil, {
       where: where,
     });
+
+    const findedDataIncludeNull = resultRowsData.map((row) => {
+      const matchRow = wantFindIndex.filter((i) => {
+        const whereOptionContent = where[String(titles[i])];
+        if (isDict(whereOptionContent))
+          return isFilterConditionsMatch(row.row[i], whereOptionContent);
+
+        return row.row[i] === whereOptionContent;
+      });
+
+      if (matchRow.length === wantFindIndex.length) return row;
+
+      return null;
+    });
+
+    const findedData = findedDataIncludeNull.filter((data) => data !== null);
+
+    if (whereArrayIndex === 0) {
+      resultRowsData = findedData;
+      return;
+    }
+
+    const alreadyHitRowNumbers = resultRowsData.map((row) => row.rowNumber);
+
+    const newInsertedArray = findedData.filter(
+      (row) => !alreadyHitRowNumbers.includes(row.rowNumber)
+    );
+
+    resultRowsData = resultRowsData.concat(newInsertedArray);
+
+    if ("OR" in where || "AND" in where || "NOT" in where) {
+      const orAndNotRowsData = isLogicMatch(
+        findedData,
+        where,
+        titles,
+        gassmaControllerUtil
+      );
+
+      const alreadyHitRowNumbers = resultRowsData.map((row) => row.rowNumber);
+      const newInsertedArray = orAndNotRowsData.filter(
+        (row) => !alreadyHitRowNumbers.includes(row.rowNumber)
+      );
+
+      resultRowsData = resultRowsData.concat(newInsertedArray);
+    }
   });
 
-  return [[]];
+  return resultRowsData;
 };
 
 export { isOrMatch };
