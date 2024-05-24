cfa71870b931e2bb12eb8f1060c7e114f354dc00
akahoshi1421 <akaho1421@gmail.com>
2024-05-25 04:35:34 +0900

    change: deleteManyをwhereFilterに置き換え

--- src/util/delete/deleteMany.ts
+++ src/util/delete/deleteMany.ts
@@ -1,10 +1,6 @@
 import { DeleteData } from "../../types/findTypes";
 import { GassmaControllerUtil } from "../../types/gassmaControllerUtilType";
-import { getAllData } from "../core/getAllData";
-import { getTitle } from "../core/getTitle";
-import { getWantFindIndex } from "../core/getWantFindIndex";
-import { isFilterConditionsMatch } from "../filterConditions/filterConditions";
-import { isDict } from "../other/isDict";
+import { whereFilter } from "../core/whereFilter";
 
 const deleteManyFunc = (
   gassmaControllerUtil: GassmaControllerUtil,
@@ -14,25 +10,11 @@ const deleteManyFunc = (
 
   const where = deleteData.where;
 
-  const wantFindIndex = getWantFindIndex(gassmaControllerUtil, deleteData);
-
-  const allDataList = getAllData(gassmaControllerUtil);
-  const titles = getTitle(gassmaControllerUtil);
-
+  const findedData = whereFilter(where, gassmaControllerUtil);
   let deletedCnt = 0;
 
-  allDataList.forEach((row, rowIndex) => {
-    const matchRow = wantFindIndex.filter((i) => {
-      const whereOptionContent = where[String(titles[i])];
-      if (isDict(whereOptionContent))
-        return isFilterConditionsMatch(row[i], whereOptionContent);
-
-      return row[i] === whereOptionContent;
-    });
-
-    if (matchRow.length !== wantFindIndex.length) return;
-
-    sheet.deleteRow(rowIndex + 1 + startRowNumber + deletedCnt);
+  findedData.forEach((row) => {
+    sheet.deleteRow(row.rowNumber + startRowNumber + deletedCnt);
     deletedCnt--;
   });
 };
