d94c48b33c9010afc63ca3535be9305737f40e87
akahoshi1421 <akaho1421@gmail.com>
2024-06-02 04:12:52 +0900

    add: added count method

---
+++ src/types/countType.ts
@@ -0,0 +1,10 @@
+import { OrderBy, WhereUse } from "./coreTypes";
+
+type CountData = {
+  where?: WhereUse;
+  orderBy?: OrderBy | OrderBy[];
+  take?: number;
+  skip?: number;
+};
+
+export { CountData };
