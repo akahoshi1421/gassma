89f23295ca2a1944a6ef0a2c64f1417e0f897a76
akahoshi1421 <akaho1421@gmail.com>
2025-05-01 23:38:24 +0900

    add: otherCreate (filterCondtions)

--- src/util/validation/otherCreate.ts
+++ src/util/validation/otherCreate.ts
@@ -1,11 +1,22 @@
 import { GassmaValidationErrror } from "../../errors/validation/validationError";
 import { SchemaType } from "../../types/core/schemaType";
 import { AnyUse } from "../../types/coreTypes";
+import { isDict } from "../other/isDict";
 
 const otherCreateValidation = (validation: AnyUse, schema: SchemaType) => {
   Object.keys(validation).forEach((key) => {
+    if (key === "AND" || key === "OR" || key === "NOT") return;
+
+    const value = validation[key];
+
     const oneSchema = schema.shape[key];
     if (!oneSchema) throw new GassmaValidationErrror(key);
+
+    if (isDict(value)) {
+      return Object.keys(value).forEach((filterConditionsKey) => {
+        oneSchema.parse(value[filterConditionsKey]);
+      });
+    }
 
     oneSchema.parse(validation[key]);
   });
